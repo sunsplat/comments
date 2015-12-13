16eca93d84a6eaf2392e3d50019a7327771b4280
sunsplat <heeycutie@yahoo.com>
2015-12-13 13:17:20 +0800

    finished

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,19 +1,56 @@
 // return()  always needs parantheses
 
-
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 // tutorial1.js
 var CommentBox = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	handleCommentSubmit: function(comment) {
+		var comments = this.state.data;
+		comment.id = Date.now();
+		var newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+
+		$.ajax({
+		url: this.props.url,
+		dataType: 'json',
+		type: 'POST',
+		data: comment,
+		success: function(data) {
+			this.setState({data: data});
+		}.bind(this),
+		error: function(xhr, status, err) {
+			this.setState({data: comments});
+			console.error(this.props.url, status, err.toString());
+		}.bind(this)
+	});
+	},
+
+	// executes exactly once during the lifecycle of the component and sets up the initial state of the component.
+	getInitialState: function() {
+    return {data: []};
+  },
+
+	componentDidMount: function() {
+		this.loadCommentsFromServer();	
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
@@ -38,11 +75,32 @@ var CommentBox = React.createClass({
 	});
 
 	var CommentForm = React.createClass({
+			getInitialState: function() {
+				return {author: '', text: ''};
+			},
+			handleAuthorChange: function(e) {
+				this.setState({author: e.target.value});
+			},
+			handleTextChange: function(e) {
+				this.setState({text: e.target.value});
+			},
+		handleSubmit: function(e) {
+			e.preventDefault();
+			var author = this.state.author.trim();
+			var text = this.state.text.trim();
+			if (!text || !author) {
+				return;
+			}
+			this.props.onCommentSubmit({author: author, text: text})
+			this.setState({author: '', text: ''});
+		},
 	  render: function() {
 	    return (
-	      <div className="commentForm">
-	        Hello, world! I am a CommentForm.
-	      </div>
+	      <form className="commentForm" onSubmit={this.handleSubmit}>
+	      	<input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+	      	<input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+	      	<input type="submit" value="Post" />
+	      </form>
 	    );
 	  }
 	});
@@ -67,6 +125,6 @@ var Comment = React.createClass({
 
 
 ReactDOM.render(
-  <CommentBox url="/api/comments" />,
+  <CommentBox url="/api/comments" pollInterval={2000} />,
   document.getElementById('content')
 );
